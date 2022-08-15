dbaada80be0730bf51fbaca36f33eaca8e52d2a4
nozer <93734919+nozer-p@users.noreply.github.com>
2022-08-15 16:50:45 +0300

    anim_v2

--- Slow Box/Assets/Scripts/PlayerMovement.cs
+++ Slow Box/Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,53 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private float timeBtwPunch;
+    [SerializeField] private float startTimeBtwPunch;
+
+    [SerializeField] private Transform punchPos;
+    [SerializeField] private float punchRange;
+    [SerializeField] private LayerMask enemy;
+    [SerializeField] private float damage;
+    private Animator animator;
+
+    private List<Collider> enemies = new List<Collider>();
 
     void Start()
     {
-        
+        timeBtwPunch = 0f;
+        animator = GetComponent<Animator>();
     }
 
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Punch();
+    }
+
+    private void Punch()
+    {
+        if (timeBtwPunch <= 0f)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                animator.SetTrigger("punch");
+                enemies.AddRange(Physics.OverlapSphere(punchPos.position, punchRange, enemy));
+                foreach (Collider en in enemies)
+                {
+                    Debug.Log(damage);
+                }
+            }
+
+            timeBtwPunch = startTimeBtwPunch;
+        }
+        else
+        {
+            timeBtwPunch -= Time.deltaTime;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(punchPos.position, punchRange);
     }
 }
